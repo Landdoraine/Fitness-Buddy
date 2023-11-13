98795609e07945813dc1f3eaeb18c562ac4c8a00
Landdoraine Chen <wending.chen2020@gmail.com>
2023-11-13 16:50:38 -0500

    Heroku Tinkering

--- server/server.js
+++ server/server.js
@@ -24,16 +24,11 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.log('connected to db & listening on port', port)
     })
   })
   .catch((error) => {
     console.log(error)
-  })
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  });
