c83a40ea984e8e23741b010953fad17824184363
Landdoraine Chen <wending.chen2020@gmail.com>
2023-11-13 17:20:36 -0500

    Heroku Tinkering

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Server is up!");
+});
+
+module.exports = router;
 // express app
 const app = express()
 
